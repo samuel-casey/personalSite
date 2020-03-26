ffc0c2c7b483a4eeab72463b7a9a8149963d025c
s-c <48266366+s-c-@users.noreply.github.com>
2020-03-26 10:01:20 -0400

    Mar 26 AM, no server yet

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,4 @@
-const functions = require('firebase-functions');
 const express = require('express');
-const path = require('path');
+
 
 const app = express();
-// app.get('/', (request, response) => {
-//     let fileName = "adminwp.html"
-//     const file = path.join(__dirname, "/public", `/${fileName}`)
-//     if (file) {
-//     response.sendFile(file)
-//     } else {
-//         response.send("no msg")
-//     }
-// });
-
-app.use(express.static(path.join(__dirname, "../public")))
-
-const PORT = process.env.PORT || 5015;
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.app = functions.https.onRequest(app)
--- public/admin.js
+++ public/admin.js
@@ -1,17 +1,3 @@
-const firebase = require('firebase');
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCIGp60bXHkC7ssMBFX5sAtBnD6vbIpfWU",
-    authDomain: "personalsite-35a38.firebaseapp.com",
-    databaseURL: "https://personalsite-35a38.firebaseio.com",
-    projectId: "personalsite-35a38",
-    storageBucket: "personalsite-35a38.appspot.com",
-    messagingSenderId: "933460557734",
-    appId: "1:933460557734:web:714421b5ce76c1ad9bd967",
-    measurementId: "G-3BHK9Q0JWL"
-  };
-  
-  firebase.initializeApp(config);
 
   const email = document.querySelector('.email').value
   const password = document.querySelector('.password').value
