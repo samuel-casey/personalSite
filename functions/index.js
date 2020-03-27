1fa038218ea8f0d784a76f224dcb4338bed20de1
s-c <48266366+s-c-@users.noreply.github.com>
2020-03-27 10:50:59 -0400

    first commit, adding back appropriate .ejs and .js files

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,21 @@
+  
+const functions = require('firebase-functions');
 const express = require('express');
-
+const engines = require('consolidate')
 
 const app = express();
+
+app.engine('ejs', engines.ejs)
+app.set('views', __dirname + '/html')
+app.set('view engine', 'ejs')
+
+
+app.get('/admin', function(request, response) {
+  response.render('admin');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+exports.app = functions.https.onRequest(app);
