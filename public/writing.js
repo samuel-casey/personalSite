fe9dfbd313fe70f6e70139e40790fc02ff8f34c1
s-c <48266366+s-c-@users.noreply.github.com>
2020-03-31 11:27:21 -0400

    created writing upload functionality

--- public/writing.js
+++ public/writing.js
@@ -1,29 +1,53 @@
+
+
 $(document).ready(() => {
     var loader = "<div id='loadBox'><div class='loader'></div><br><p>loading writing...</p></div>"
     $("#base").append(loader)
 })
 
-let postContent = {
-    title : "title",
-    body : "body"
+$(document).on('load', getWriting().then(displayWriting))
+
+function displayWriting() {
+
+
+    
+
+    $("#base").detach()
+    // loop to attach new containers for length of post list
+    for (var i = 0; i < postList.length; i++) {
+
+        var postContent = {
+            title : postList[i].title,
+            body : postList[i].body
+        }
+
+        var newPost = `<div class="container onePost">
+        <h1 class="postTitle">${postContent.title}</h1>
+        <div class="cotainer-fluid postBG"> 
+            <p class="postBody" id="${i}">${postContent.body}</p> 
+        </div>
+    </div>`
+
+        $("#allPosts").append(newPost)
+    }
+    
+    
 }
 
-// $(document).on('load', loadWriting())
+var postList = []
 
-function loadWriting() {
-    console.log(`${postContent}`)
-}
-
-function getPosts() {
-    return firebase.database().ref("posts").once('value').then(function(snapshot) {
+function getWriting() {
+    return firebase.database().ref("writing").once('value').then(function(snapshot) {
         var posts = snapshot.val();
         console.log(posts)
-    
-        var counter = 1
+
         for (var postKey in posts) {
-          var post = posts[postKey];
-          $("#post-listing").append(`<div>Post number:${counter}| Post title:${post.title} | Post body:${post.body}</div>`)
-          counter += 1 
-        }
+            var post = posts[postKey];
+            postList.push(post)
+          }
+        console.log(postList)
       });  
+
+      
+   
 }
